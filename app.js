067d3968bb4add8df1d0bbe73b696189fdef5b98
jgsjss9854 <87510168+jgsjss@users.noreply.github.com>
2022-03-23 15:39:13 +0900

    qwe

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.listen(5000, function (req, res){
+  console.log("서버시작 : ", 5000)
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
